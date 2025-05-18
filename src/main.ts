a401bf92779e671d2975e51855fd6c2de1a2f91a
Jonathan Hernandez Sanchez <jhonyhs95@outlook.com>
2025-05-18 11:09:38 -0600

    feat: implement order creation and retrieval functionality

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { envs } from './config';
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const logger = new Logger('Main');
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.TCP,
+      options: {
+        port: envs.port,
+      },
+    },
+  );
 
-  await app.listen(envs.port ?? 3000);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
 
-  logger.log(`Gateway runningon port ${envs.port}`);
+  logger.log(`Microservices runningon port ${envs.port}`);
 }
-bootstrap();
+void bootstrap();
