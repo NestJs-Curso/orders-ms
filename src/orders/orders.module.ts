f44753409d7da5fd1ec3c6486787d335d418b4fa
Jonathan Hernandez Sanchez <jhonyhs95@outlook.com>
2025-06-17 12:34:20 -0600

    feat: refactor NATS integration into separate NatsModule for better modularity

--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -1,22 +1,11 @@
 import { Module } from '@nestjs/common';
 import { OrdersService } from './orders.service';
 import { OrdersController } from './orders.controller';
-import { ClientsModule, Transport } from '@nestjs/microservices';
-import { envs, NATS_SERVICE } from 'src/config';
+import { NatsModule } from 'src/transports/nats.module';
 
 @Module({
   controllers: [OrdersController],
   providers: [OrdersService],
-  imports: [
-    ClientsModule.register([
-      {
-        name: NATS_SERVICE,
-        transport: Transport.NATS,
-        options: {
-          servers: envs.nats_servers,
-        },
-      },
-    ]),
-  ],
+  imports: [NatsModule],
 })
 export class OrdersModule {}
